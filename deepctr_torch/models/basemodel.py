c4e88a982bfff74be5e7f706409d5cb681d298a6
felixchu-xogo <felix.chu@appier.com>
2022-09-22 16:15:17 +0800

    update callbacks remove keras dependency

--- deepctr_torch/models/basemodel.py
+++ deepctr_torch/models/basemodel.py
@@ -19,16 +19,17 @@ from sklearn.metrics import *
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-try:
-    from tensorflow.python.keras.callbacks import CallbackList
-except ImportError:
-    from tensorflow.python.keras._impl.keras.callbacks import CallbackList
+# try:
+#     from tensorflow.python.keras.callbacks import CallbackList
+# except ImportError:
+#     from tensorflow.python.keras._impl.keras.callbacks import CallbackList
 
 from ..inputs import build_input_features, SparseFeat, DenseFeat, VarLenSparseFeat, get_varlen_pooling_list, \
     create_embedding_matrix, varlen_embedding_lookup
 from ..layers import PredictionLayer
 from ..layers.utils import slice_arrays
 # from ..callbacks import History
+from ..callbacks import CallbackList
 
 
 class Linear(nn.Module):
